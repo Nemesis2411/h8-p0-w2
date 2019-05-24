33715840cb53521d32274585284683ec1538525f
Nemesis2411 <tominugrohoedward@gmail.com>
2019-05-23 23:07:40 -0700

    tugas update

--- exercise7-w2.js
+++ exercise7-w2.js
@@ -1,7 +1,25 @@
 //soal 1
-var rows1 = 5
+var rows1 = 5;
 for (i = 1; i <= rows1; i++) {
   console.log('*')
 }
 
 //soal 2
+
+var rows2 = 5;
+var bintang = '';
+for (i = 0; i < rows2; i++) {
+  for (j = 0; j < rows2; j++) {
+    bintang = bintang + '*';
+  }
+  console.log(bintang);
+  bintang = '';
+}
+
+//soal 3
+var bintang = ''
+var rows2 = 5;
+  for (i = 0; i < rows2; i++) {
+    bintang += '*'
+     console.log(bintang) 
+  }  
