740c59b07d5a4f796958ee1fe6a9dbc90560f455
Nemesis2411 <tominugrohoedward@gmail.com>
2019-05-25 00:22:26 -0700

    tugas 13

---
+++ exercise13-w2.js
@@ -0,0 +1,24 @@
+function xo(str) {
+    // you can only write your code here!
+    var countX=0
+    var countO=0
+  for (var i = str.length-1; i>=0; i--) {//for (var i = 0; i<str.length; i++)
+if(str[i]=="x"){
+    countX++
+}
+else{
+    countO++
+}
+  }
+  if(countO==countX){
+      return true
+  }else{
+      return false
+  }
+}
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
